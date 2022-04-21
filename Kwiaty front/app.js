194dfaca65df23085803860028145fab5b93c574
MJ-228748 <228748@edu.p.lodz.pl>
2022-04-21 14:36:30 +0200

    Commit #4 - "Basic login/register system (to debug)"

--- Kwiaty front/app.js
+++ Kwiaty front/app.js
@@ -15,6 +15,7 @@ function show(elementID) {
 
 
 localStorage.setItem('lang','en')
+localStorage.setItem('auth','')
 function swap_lang(){
     if(localStorage.getItem('lang')==="en") {
         localStorage.setItem('lang','pl')
@@ -26,15 +27,7 @@ function swap_lang(){
     }
     Flowers()
 }
-function create_lang() {
-    var button = document.createElement('button');
-    button.textContent = localStorage.getItem('lang');
-    button.setAttribute('id', 'l');
-    button.setAttribute('type', 'button');
-    button.setAttribute('class', 'language');
-    button.setAttribute('onclick', 'swap_lang()');
-    document.getElementById('1').appendChild(button);
-}
+
 function get(){
     var idd=document.getElementById("flower_id").value;
     fetch('http://localhost:8080/api/product/'+idd,{method:'GET',headers:{
@@ -51,7 +44,7 @@ function put(){
     fetch('http://localhost:8080/api/product',{method:'POST',
         headers:{
             'Content-Type': 'application/json',
-            'Authorization': 'Login to do, for now just paste your own'
+            'Authorization': localStorage.getItem('auth')
         },
         body: JSON.stringify({
             descriptionEn: desc,
@@ -88,9 +81,12 @@ function displayFlowers(data) {
     const descr = document.createElement("p");
     descr.innerHTML = desc;
     flowerDiv.appendChild(descr);
+    flowerDiv.appendChild(document.createElement("br"))
     const prc = document.createElement("p");
     prc.innerHTML = rc;
     flowerDiv.appendChild(prc);
+    flowerDiv.appendChild(document.createElement("br"))
+    flowerDiv.appendChild(document.createElement("br"))
 }
 
 function displayFlower(data,id) {
@@ -128,9 +124,11 @@ function displayFlower(data,id) {
     const descr = document.createElement("p");
     descr.innerHTML = desc;
     flowerDiv.appendChild(descr);
+    flowerDiv.appendChild(document.createElement("br"))
     const prc = document.createElement("p");
     prc.innerHTML = rc;
     flowerDiv.appendChild(prc);
+    flowerDiv.appendChild(document.createElement("br"))
 }
 
 function Flowers(){
@@ -141,4 +139,64 @@ function Flowers(){
         }})
         .then(res => res.json())
         .then(data=>{console.log(data);for(let i=0; i<data.length;i++){displayFlowers(data[i])}})
+}
+
+function register(){
+    document.getElementById("Reg_res").innerHTML=""
+    var nam=document.getElementById("name").value;
+    var surnam=document.getElementById("surname").value;
+    var mail=document.getElementById("email").value;
+    var pass=document.getElementById("pass").value;
+    var pass2=document.getElementById("pass2").value;
+    if(pass2!==pass){
+        document.getElementById("Reg_res").className="error"
+        document.getElementById("Reg_res").innerHTML="Passwords do not match!"
+        return
+    }
+
+    fetch('http://localhost:8080/auth/register',{method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: mail,
+            name: nam,
+            password: pass,
+            surname: surnam
+        })
+    })
+        .then(res => {return res.json()})
+        .then(data=>console.log(data))
+    document.getElementById("Reg_res").className="success"
+    document.getElementById("Reg_res").innerHTML="Registered"
+    setTimeout(show, 3000,'main')
+}
+
+function login(){
+    document.getElementById("Log_res").innerHTML=""
+    var mail=document.getElementById("lemail").value;
+    var pass=document.getElementById("lpass").value;
+    fetch('http://localhost:8080/auth/authenticate',{method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: mail,
+            password: pass,
+        })
+    })
+        .then(res => {return res.json()})
+        .then(data=>localStorage.setItem('auth',data.jwt))
+    document.getElementById("Log_res").className="success"
+    document.getElementById("Log_res").innerHTML="Logged in"
+    document.getElementById('auth').style.display = 'none'
+    document.getElementById('auth2').style.display = 'block'
+    setTimeout(show, 3000,'main')
+}
+
+function logout(){
+    localStorage.setItem('auth','')
+    document.getElementById('auth').style.display = 'block'
+    document.getElementById('auth2').style.display = 'none'
+    setTimeout(show, 3000,'main')
 }
