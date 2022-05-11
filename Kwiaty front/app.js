065e738aaac5b00eeee782eebf47a930641cbf18
MJ-228748 <228748@edu.p.lodz.pl>
2022-05-11 18:25:46 +0200

    Commit #10 - "Cart implementation"

--- Kwiaty front/app.js
+++ Kwiaty front/app.js
@@ -10,7 +10,7 @@ function show(elementID) {
         pages[i].style.display = 'none';
     }
     ele.style.display = 'block';
-    if (elementID === "main") {
+    if (elementID === "flowerbed") {
         document.getElementById("flowerbed").style.display = 'block'
     }
 }
@@ -111,7 +111,6 @@ function displayFlower(data, id) {
     const prc = document.createElement("p");
     prc.innerHTML = rc;
     flowerDiv.appendChild(prc);
-    flowerDiv.appendChild(document.createElement("br"))
 }
 
 function swap_page(id) {
@@ -141,10 +140,10 @@ function FlowersMain() {
             }
         })
         .then(res => res.json())
-        .then(data => {console.log(data); for (let i = 0; i < data.data.length; i++) { displayFlowersMain(data.data[i], i) }for (let i = 0; i < ((data.count) / 10 ); i++) { add_page_button(i) } })
+        .then(data => {console.log(data); for (let i = 0; i < data.data.length; i++) { displayFlowersMain(data.data[i]) }for (let i = 0; i < ((data.count) / 10 ); i++) { add_page_button(i) } })
 }
 
-function displayFlowersMain(data, id) {
+function displayFlowersMain(data) {
     let name, desc, rc, imag;
     console.log(data)
     if (data.name === null) {
@@ -166,20 +165,35 @@ function displayFlowersMain(data, id) {
     const flower = document.createElement("div");
 
     flower.className = "flowerbed"
-    flower.id = "F" + id
+    flower.id = data.id
     flower.onmouseover = function() { this.style.opacity = "100%" }
     flower.onmouseout = function() { this.style.opacity = "45%" }
     const imagd = document.createElement("img");
     const named = document.createElement("h3");
     const descd = document.createElement("p");
     const priced = document.createElement("p");
+    const qnt= document.createElement("input")
+    const add= document.createElement("button")
 
     imagd.src = imag;
     imagd.className = "FlowerImg"
+
     named.innerHTML = name;
+
     descd.innerHTML = desc;
+
     if (localStorage.getItem('lang') === "en") priced.innerHTML = rc + " PLN";
     else priced.innerHTML = rc + " ZL";
+
+    qnt.type="number"
+    qnt.id="FQ"+data.id;
+    qnt.name="quantity"
+    qnt.min=0
+    qnt.defaultValue=0
+    qnt.style="width: 50px"
+
+    add.innerHTML="Add to cart"
+    add.onclick=function(){add_to_cart(data.id)}
 
 
     flower.appendChild(named);
@@ -187,6 +201,8 @@ function displayFlowersMain(data, id) {
     flower.appendChild(descd);
     flower.appendChild(document.createElement("br"))
     flower.appendChild(priced);
+    flower.appendChild(qnt);
+    flower.appendChild(add);
     flowerDiv.appendChild(flower);
 
 
@@ -221,7 +237,7 @@ function register() {
         .then(data => console.log(data))
     document.getElementById("Reg_res").className = "success"
     document.getElementById("Reg_res").innerHTML = "Registered"
-    setTimeout(show, 3000, 'main')
+    setTimeout(show, 3000, 'flowerbed')
 }
 
 function login1() {
@@ -248,6 +264,7 @@ function login2(auth) {
         document.getElementById("Log_res").innerHTML = "Incorrect email or password!"
         return
     }
+    console.log(auth)
     localStorage.setItem('token',auth)
     fetch('http://localhost:8080/auth/me', {
         method: 'GET',
@@ -266,14 +283,14 @@ function login3(user){
     document.getElementById('auth').style.display = 'none'
     document.getElementById('Greeting').innerHTML="Hello "+user.name + ' '+ user.surname+'!'
     document.getElementById('auth2').style.display = 'block'
-    setTimeout(show, 3000, 'main')
+    setTimeout(show, 3000, 'flowerbed')
 }
 
 function logout() {
     localStorage.setItem('token', '')
     document.getElementById('auth').style.display = 'block'
     document.getElementById('auth2').style.display = 'none'
-    setTimeout(show, 3000, 'main')
+    setTimeout(show, 3000, 'flowerbed')
 }
 
 function show_menu() {
@@ -281,4 +298,148 @@ function show_menu() {
     if (menu.style.display === 'block') menu.style.display = 'none';
     else if (menu.style.display === 'none') menu.style.display = 'block';
 
+}
+
+function get_cart() {
+    const cartDiv = document.getElementById("cart");
+    cartDiv.innerHTML = ""
+    if (localStorage.getItem('token') !== '') {
+        fetch('http://localhost:8080/cart', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                'Accept-Language': localStorage.getItem('lang')
+            }
+        })
+            .then(res => {
+                return res.json()
+            })
+            //.then(data => function (){if(data)})
+            .then(data => {
+                if (data.length > 0) {
+                    for (let i = 0; i < data.length; i++) {
+                        displaycart(data[i])
+                    }
+                    const clear = document.createElement("button")
+                    clear.innerHTML = "Clear Cart"
+                    clear.onclick = function (){clear_cart()}
+                    cartDiv.appendChild(clear);
+                    cartDiv.appendChild(document.createElement("br"))
+                    const exit = document.createElement("button")
+                    exit.textContent = "Back"
+                    exit.onclick=function () {show('flowerbed')}
+                    cartDiv.appendChild(exit);
+                } else {
+                    cartDiv.innerHTML = "Cart is empty";
+                    cartDiv.appendChild(document.createElement("br"))
+                    const exit = document.createElement("button")
+                    exit.textContent = "Back"
+                    exit.onclick=function () {show('flowerbed')}
+                    cartDiv.appendChild(exit);
+                }
+            })
+    }
+    else{
+        document.getElementById("cart").innerHTML = "Not Logged In";
+        document.getElementById("cart").appendChild(document.createElement("br"))
+        const exit = document.createElement("button")
+        exit.textContent = "Back"
+        exit.onclick=function () {show('flowerbed')}
+        cartDiv.appendChild(exit);
+    }
+}
+
+
+function add_to_cart(id){
+    fetch('http://localhost:8080/cart', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+localStorage.getItem('token'),
+        },
+        body: JSON.stringify({
+            product: id,
+            quantity: document.getElementById(("FQ"+id)).value
+        })
+    })
+        .then(res => { return res.json() })
+}
+function displaycart(data){
+    let name, desc, rc, imag;
+    if (data.product.name === null) {
+        if (localStorage.getItem('lang') === "en") name = "No name set"
+        else name = "Brak nazwy"
+    } else name = data.product.name
+    if (data.product.description === null) {
+        if (localStorage.getItem('lang') === "en") desc = "No description set"
+        else desc = "Brak opisu"
+    } else desc = data.product.description
+    if (data.product.price === null) {
+        if (localStorage.getItem('lang') === "en") rc = "No price set"
+        else rc = "Brak ceny"
+    } else rc = data.product.price
+    if (data.product.images.length === 0) imag = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F002%2F261%2F129%2Foriginal%2Fno-sign-empty-red-crossed-out-circle-not-allowed-sign-isolate-on-white-background-illustration-eps-10-free-vector.jpg&f=1&nofb=1"
+    else imag = data.product.images[0].url
+    const cartdiv = document.getElementById("cart");
+
+    const flower = document.createElement("div");
+
+    flower.className = "flowerbed"
+    flower.id = data.product.id
+    flower.onmouseover = function() { this.style.opacity = "100%" }
+    flower.onmouseout = function() { this.style.opacity = "45%" }
+    const imagd = document.createElement("img");
+    const named = document.createElement("h3");
+    const descd = document.createElement("p");
+    const priced = document.createElement("p");
+    const del = document.createElement("button")
+
+
+    imagd.src = imag;
+    imagd.className = "FlowerImg"
+
+    named.innerHTML = data.quantity+"x"+name;
+
+    descd.innerHTML = desc;
+
+    del.innerHTML="Remove from cart"
+    del.onclick=function(){remove_from_cart(data.product.id);get_cart()}
+
+    if (localStorage.getItem('lang') === "en") priced.innerHTML=rc +"( "+data.quantity*rc+") " +" PLN";
+    else priced.innerHTML = rc +"( "+data.quantity*rc+") " +" ZL";
+
+
+
+    flower.appendChild(named);
+    flower.appendChild(imagd);
+    flower.appendChild(descd);
+    flower.appendChild(document.createElement("br"))
+    flower.appendChild(priced);
+    flower.appendChild(del)
+    cartdiv.appendChild(flower);
+}
+
+function remove_from_cart(id){
+    fetch('http://localhost:8080/cart/'+id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+localStorage.getItem('token'),
+        },
+    })
+        .then(res => { return res.json() })
+        .then(()=>get_cart())
+}
+
+function clear_cart(){
+    fetch('http://localhost:8080/cart/', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+localStorage.getItem('token'),
+        },
+    })
+        .then(res => { return res.text() })
+        .then(()=>get_cart())
 }
